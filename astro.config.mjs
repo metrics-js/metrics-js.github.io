8322f2a5b9ecdba3139a97508341c39ca152bd18
William Killerud <william.killerud@m10s.io>
2025-12-12 14:47:42 +0100

    chore: update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,40 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'MetricsJS',
-			logo: { src: './src/assets/metrics-js-logo.svg' },
-			customCss: ['./src/styles/theme.css'],
-			social: {
-				github: 'https://github.com/metrics-js/',
-			},
-			editLink: {
-				baseUrl: 'https://github.com/metrics-js/metrics-js.github.io/edit/main',
-				pattern: 'docs',
-			},
-			sidebar: [
-				{
-					label: 'Introduction',
-					items: [
-						{ label: 'Getting started', link: '/introduction/getting-started/' },
-						{ label: 'Why MetricsJS', link: '/introduction/why-metricsjs/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "MetricsJS",
+      logo: { src: "./src/assets/metrics-js-logo.svg" },
+      customCss: ["./src/styles/theme.css"],
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/metrics-js/",
+        },
+      ],
+      editLink: {
+        baseUrl: "https://github.com/metrics-js/metrics-js.github.io/edit/main",
+        pattern: "docs",
+      },
+      sidebar: [
+        {
+          label: "Introduction",
+          items: [
+            {
+              label: "Getting started",
+              link: "/introduction/getting-started/",
+            },
+            { label: "Why MetricsJS", link: "/introduction/why-metricsjs/" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
